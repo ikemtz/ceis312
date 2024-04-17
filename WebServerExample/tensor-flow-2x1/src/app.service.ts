74d0e3e6b679b5635f171e1db72fc280be740328
Isaac Martinez (@ikemtz) <ikemtz@outlook.com>
2024-04-17 12:41:00 -0400

    Adding tensorflow-js project

--- WebServerExample/tensor-flow-2x1/src/app.service.ts
+++ WebServerExample/tensor-flow-2x1/src/app.service.ts
@@ -1,13 +1,11 @@
 import { Injectable } from '@nestjs/common';
-import { join } from 'path';
 import * as tf from '@tensorflow/tfjs';
 
 @Injectable()
 export class AppService {
   async getModel(): Promise<tf.LayersModel> {
-    const filePath = join('file://', __dirname, './src/models/2x1.json');
-    console.log(filePath);
-    const model = await tf.loadLayersModel(filePath);
+    const model = await tf.loadLayersModel(
+      'https://raw.githubusercontent.com/ikemtz/ceis312/master/WebServerExample/tensor-flow-2x1/src/models/2x1.json');
     return model;
   }
 }
